e94715027d6e08fd715dc543b1874037a8101af2
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2017-09-04 00:35:28 +0300

    unnecessary node install method deleted

--- node/install-v1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
-sudo apt-get update
-sudo apt-get install build-essential libssl-dev
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-source ~/.profile
-nvm install node
-nvm use node
-node -v
